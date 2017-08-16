04e8034f0cfcd7bc0486077f674227e1ceeca298
infinityAlive <sonic@ehanlin.com.tw>
2017-08-16 12:59:12 +0800

    網頁使用裝置與視窗大小的警告訊息改為用 js 判斷

--- babel-temp/js/main.js
+++ babel-temp/js/main.js
@@ -1,6 +1,53 @@
 "use strict";
 
 require(["config"], function (config) {
+  var determineDevice = function determineDevice() {
+    var determineOrientation = function determineOrientation() {
+      var limitedSizeStyle = document.getElementById("limitedSize").style;
+      var deviceDirection = Math.abs(window.orientation) === 90 ? "landscape" : "portrait";
+
+      if (deviceDirection === "portrait") {
+        limitedSizeStyle.display = "block";
+      }
+      if (deviceDirection === "landscape") {
+        limitedSizeStyle.display = "none";
+      }
+    };
+
+    /*
+     * 視窗調整，以限定尺寸為標準值，控制是否跳出提醒畫面，
+     * 以 flag 紀錄，此後調整視窗若還是在該限定尺寸範圍，便不用重新設置
+     * 直到超出或小於限定尺寸，flag 會再重新紀錄
+     * 
+     */
+    var isDeplayBlock = false;
+    var determineWindowSize = function determineWindowSize() {
+      var limitedSizeStyle = document.getElementById("limitedSize").style;
+      if (window.innerWidth < 700 || window.innerHeigth < 480) {
+        if (!isDeplayBlock) {
+          limitedSizeStyle.display = "block";
+          isDeplayBlock = true;
+        }
+      } else {
+        if (isDeplayBlock) {
+          limitedSizeStyle.display = "none";
+          isDeplayBlock = false;
+        }
+      }
+    };
+
+    var device = navigator.userAgent;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(device)) {
+      if (window.orientation) {
+        determineOrientation();
+        window.addEventListener("onorientationchange", determineOrientation);
+      }
+    } else {
+      determineWindowSize();
+      window.addEventListener("resize", determineWindowSize);
+    }
+  };
+  determineDevice();
   require(["v_componentProgressButton"]);
   require(["v_anchorList"]);
   require(["v_start"]);
