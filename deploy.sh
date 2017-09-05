3b8c5f16d24ccd9396769b94bbf755dc0cccf630
infinityAlive <sonic@ehanlin.com.tw>
2017-09-05 12:44:15 +0800

    fix repository name

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 test="http://test.ehanlin.com.tw/event/api/Deploy"
 production="http://www.ehanlin.com.tw/event/api/Deploy"
-repository="{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}"
+repository="{\"Repository\":\"event-richman-questionnaire\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"richman-questionnaire\"}"
 contentTypeJson="Content-Type: application/json"
 
 case "${TRAVIS_TAG}" in
 *SNAPSHOT*)
 	echo "deploy to test"
-	#curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-ad-hswriting\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"ad-hswriting\"}" 'http://www.ehanlin.com.tw/event/api/Deploy'
 	curl -X POST -H "${contentTypeJson}" -d "${repository}" "${test}"
 	;;
 *)
